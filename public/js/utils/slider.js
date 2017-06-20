aa7708bf607d93bd06a9daa827fa74ffc4ec488b
lulublondet <maria.mlvp@gmail.com>
2017-06-20 18:10:58 -0500

    mi primer hito

---
+++ public/js/utils/slider.js
@@ -0,0 +1,5 @@
+var arrImagenes = [{url:"img/icons/icon-people.png", id:"1", titulo:"Paga a tus amigos", msg:"Paga a quien quieras desde donde quieras, sin usar efectivo"},
+				   {url:"img/icons/happy-person.png", id:"2", titulo:"Sin número de cuenta", msg:"Elige a quién pagar desde tu lista de contactos"},
+				   {url:"img/icons/group-people.png", id:"3", titulo:"Gratis y Seguro", msg:"La transferencia es inmediata y gratuita de una cuenta a otra"}
+				   ];
+
